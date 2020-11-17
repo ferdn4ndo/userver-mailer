2bcce375315fb7dbf213ac7d6261a62e4cc32617
Fernando Constantino <const.fernando@gmail.com>
2020-11-17 01:25:30 -0300

    split tls certs

--- webmail/config/imap.php
+++ webmail/config/imap.php
@@ -1,11 +1,19 @@
 <?php
 
+#
 $config['imap_conn_options'] = array(
-    'ssl' => array(
-      'verify_peer'       => true,
-      'allow_self_signed' => true,
-      'peer_name'         => getenv('TLS_PEER_NAME'),
-      'ciphers'           => 'TLSv1+HIGH:!aNull:@STRENGTH',
-      'cafile'            => getenv('TLS_CERT_PATH'),
-    ),
-);
+  'ssl' => array(
+    'verify_peer' => true,
+    // certificate is not self-signed if cafile provided
+    'allow_self_signed' => false,
+    // If not LetsEncrypt:
+    // 'cafile'  => '/etc/ssl/certs/Your_CA_certificate.pem',
+    // For Letsencrypt use the following two lines and remove the 'cafile' option above.
+    'ssl_cert' => getenv('TLS_CERT_PEM_FILE'),
+    'ssl_key'  => getenv('TLS_CERT_KEY_FILE'),
+    // probably optional parameters
+    'ciphers' => 'TLSv1+HIGH:!aNull:@STRENGTH',
+    'peer_name'         => getenv('TLS_PEER_NAME'),
+  )
+); 
+
