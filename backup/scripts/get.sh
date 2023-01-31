58a6948116d04b1d0675bdd370fa7af53491bc03
Fernando Constantino <const.fernando@gmail.com>
2023-01-31 20:32:24 -0300

    Migrated from debian to alpine and minor refactoring

--- backup/scripts/get.sh
+++ backup/scripts/get.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-echo "Job get started: $(date)"
+echo "Job 'get' started: $(date)"
 
 umask 0
 /usr/local/bin/s3cmd get -r $PARAMS  "$S3_PATH" "$DATA_PATH"
 
-echo "Job get finished: $(date)"
+echo "Job 'get' finished: $(date)"
